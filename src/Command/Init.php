d880d7b59761e2fa8d27c0d20fbc568a00f39106
Awakenweb <mathieu.savelli@awakenweb.fr>
2014-10-21 23:30:33 +0200

    Changed the init boilerplate generated to a more useful one

--- src/Command/Init.php
+++ src/Command/Init.php
@@ -31,9 +31,24 @@ class Init extends Command
 
 use Awakenweb\Beverage\Beverage;
 
+function defaultTask()
+{
+    minifyCss();
+    minifyJs();
+}
 
-function defaultTask(){
-    
+function minifyCss()
+{
+    Beverage::files('*.css', ['css'])
+            ->then(['Awakenweb\Beverage\Modules\Css', 'process'])
+            ->destination('build/css');
+}
+
+function minifyJs()
+{
+    Beverage::files('*.js', ['js'])
+            ->then(['Awakenweb\Beverage\Modules\Js', 'process'])
+            ->destination('build/js');
 }";
 
         $fs->dumpFile('./drinkmenu.php', $boilerplate);
